c088d82d5e835a057f5bc939ad9a2198ef802699
zh.cui <578654235@qq.com>
2023-05-04 21:54:59 +0800

    first commit

---
+++ demo/src/main/java/com/czh/reggie/demo/dao/DishFlavorDao.java
@@ -0,0 +1,7 @@
+package com.czh.reggie.demo.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.czh.reggie.demo.pojo.DishFlavor;
+
+public interface DishFlavorDao extends BaseMapper<DishFlavor> {
+}
---
+++ demo/src/main/java/com/czh/reggie/demo/service/impl/DishFlavorServiceImpl.java
@@ -0,0 +1,9 @@
+package com.czh.reggie.demo.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.czh.reggie.demo.dao.DishFlavorDao;
+import com.czh.reggie.demo.pojo.DishFlavor;
+import com.czh.reggie.demo.service.DishFlavorService;
+
+public class DishFlavorServiceImpl extends ServiceImpl<DishFlavorDao, DishFlavor> implements DishFlavorService {
+}
